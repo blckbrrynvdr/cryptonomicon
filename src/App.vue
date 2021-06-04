7dbc4d79e2cda9a8e73de7c71dd279e5cdcbdc32
Artur Vrublevskiy <v8a8rt@gmail.com>
2021-06-04 12:39:48 +0300

    DOne

--- src/App.vue
+++ src/App.vue
@@ -89,9 +89,27 @@
 
       <div v-if="tickers.length > 0">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <div>
+          <button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="page > 1"
+            @click="page = page - 1"
+          >
+            Назад
+          </button>
+          <button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="hasNextPage"
+            @click="page = page + 1"
+          >
+            Вперед
+          </button>
+          <div>Фильтр <input type="text" v-model="filter" /></div>
+        </div>
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -184,16 +202,30 @@ export default {
       tickers: [],
       sel: null,
       graph: [],
+      page: 1,
+      filter: "",
       coinsList: [],
       searchHints: [],
       searchError: {},
-      showPreloader: true
+      showPreloader: true,
+      hasNextPage: true
     };
   },
   mounted: function() {
     this.getCoinsList();
   },
   created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const tickersData = localStorage.getItem("cryptonomicon-list");
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
@@ -204,24 +236,45 @@ export default {
   },
 
   methods: {
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+      const filteredTickers = this.tickers.filter(ticker =>
+        ticker.name.includes(this.filter)
+      );
+
+      this.hasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
+    },
+
     subscribeToUpdates(tickerName) {
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=481c89e4e191dbcb0a10653d3146e9fd33795b04530f742a14497e7a9afccb70`
-        );
-        const data = await f.json();
+      let updaterInterval = setInterval(async () => {
+        // const f = await fetch(
+        //   `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=481c89e4e191dbcb0a10653d3146e9fd33795b04530f742a14497e7a9afccb70`
+        // );
+        // const data = await f.json();
+
         // из-за особенностей VUE3 тут реактивность перестаёт работать, приходится костылять
-        this.tickers.find(t => t.name === tickerName).price =
+        let thatTicker = this.tickers.find(t => t.name === tickerName);
+
+        /** Заглушка */
+        const data = { USD: Math.ceil(Math.random() * 100) };
+
+        thatTicker.price =
           data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+        /* Записываем id интервала чтоб сбросить когда надо будет */
+        thatTicker.updater = updaterInterval;
 
         if (this.sel?.name === tickerName) {
           this.graph.push(data.USD);
         }
       }, 5000);
-
       // и потом очищаем строку ввода
       this.ticker = "";
     },
+
     // добавление тикера
     add() {
       // объявляем переменную нового тикера
@@ -247,6 +300,7 @@ export default {
         return false;
       }
 
+      this.filter = "";
       // в массив тикеров пушим новый тикер
       this.tickers.push(currentTicker);
 
@@ -261,7 +315,10 @@ export default {
     },
 
     handleDelete(tickerToRemove) {
+      /* При удалении элемента останавливаем его обновление через setInterval */
+      clearInterval(tickerToRemove.updater);
       this.tickers = this.tickers.filter(t => t !== tickerToRemove);
+      /* Удаляем так же из локал стоража */
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
     },
 
@@ -317,7 +374,7 @@ export default {
         }
         return 0;
       };
-      console.log(searchedCoins.sort(sortByNames));
+
       this.searchHints = searchedCoins.sort(sortByNames).splice(0, 4);
     },
 
@@ -342,6 +399,25 @@ export default {
     addByTooltip(tickerToAdd) {
       this.ticker = tickerToAdd;
       this.add();
+    }
+  },
+
+  watch: {
+    filter() {
+      this.page = 1;
+
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
     }
   }
 };
